8f6cac89189e21b1616fbb190c05d4293ab68570
AldoArag <dorasoaragon66@gmail.com>
2026-03-22 14:16:36 -0600

    Configuración exitosa de ES Modules y servidor funcionando

--- routes/author.js
+++ routes/author.js
@@ -1,4 +1,4 @@
-var express = require('express');
+import express from 'express';
 var router = express.Router();
 
 /* AUTOR */
@@ -10,4 +10,4 @@ router.get('/', function(req, res, next) {
      });
 });
 
-module.exports = router;
+export default router;
