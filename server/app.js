bc83d431680d52a6a85706a76b9f88800fec1541
AldoArag <dorasoaragon66@gmail.com>
2026-03-25 10:40:54 -0600

    ♻️refactor: Add module imports aliases Add module imports for routing files in app.js and update package.json

--- server/app.js
+++ server/app.js
@@ -10,9 +10,9 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
-import authorRouter from './routes/author.js';
+import indexRouter from '#routes/index.js';
+import usersRouter from '#routes/users.js';
+import authorRouter from '#routes/author.js';
 var app = express();
 
 // view engine setup
