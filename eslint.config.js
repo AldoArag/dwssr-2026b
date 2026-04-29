53d976af5055d3a0d8498168cecadd57ece870a2
AldoArag <dorasoaragon66@gmail.com>
2026-04-29 10:58:35 -0600

    ✨feat CSS Framework enhance project setup with tailwind Css integration update Scripts, and add header styles

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
+
 export default defineConfig([
   //1 ignore builds del front y dependencias
   {
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-//importo la funcion de configuracion de vite
-import {defineConfig } from 'vite'
-//importo un resolvedor de rutas
-import {resolve} from "node:path"
+// Importo la funcion de configuracion de vite
+import{ defineConfig } from 'vite'
+// Importo un resolvedor de rutas
+import { resolve } from "node:path"
+//import tailwind
+import tailwindcss from "@tailwindcss/vite";
 
-// Exportar una instancia de configuracion
+// Exportar una instancia de configuracion 
 export default defineConfig({
     //Directorio raiz de los archivos fuente
     root: 'src',
+    //plugins
+    plugins: [tailwindcss()],
     //Configuracion del servidor
+    //de desarrollo de front-end
     server: {
         port: 5173,
         strictPort: true    
