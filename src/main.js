53019bfe8c96ebf219a7dfcdcf2bdea08048b840
AldoArag <dorasoaragon66@gmail.com>
2026-05-06 09:46:41 -0600

    ✨Feat: Prettier formal JavaScript files and add Prettier configuration

--- src/main.js
+++ src/main.js
@@ -3,4 +3,4 @@ import './styles/main.css'
 import "./styles/header.css"
 
 //Scripts principales
-console.log("🎉 Vite + Express + Handlebards funcionionando")
+console.log('🎉 Vite + Express + Handlebards funcionionando');
